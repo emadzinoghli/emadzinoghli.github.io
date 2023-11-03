7f0c7754a54570cde6fa4a3cb95ddf67f0b5f79c
Alxelr2014 <emadzinoghli@gmail.com>
2023-11-03 12:16:44 +0330

    google analytics

---
+++ _includes/scripts/googleAnalytics.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FLRL0SLT4Q"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FLRL0SLT4Q');
+</script>
