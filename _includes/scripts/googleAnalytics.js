0b91c944d6a81445286aa6aace8b642bc5754749
Alxelr2014 <emadzinoghli@gmail.com>
2023-11-06 18:48:19 +0330

    fix google analytics

--- _includes/scripts/googleAnalytics.js
+++
@@ -1,9 +0,0 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-FLRL0SLT4Q"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-FLRL0SLT4Q');
-</script>
